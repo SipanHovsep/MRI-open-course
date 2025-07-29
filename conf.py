ec4c977bfcefa28775e6ee2364f0b2a31360741b
Sipan Hovsepian <sh7437@nyu.edu>
2025-07-29 18:04:58 +0400

    update

--- conf.py
+++ conf.py
@@ -13,8 +13,7 @@ author = 'Sipan Hovsepian'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
-source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
